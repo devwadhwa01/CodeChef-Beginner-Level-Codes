a9478e77809ae49289961d53f161b5723f66805c
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-05-12 19:20:48 +0530

    Update INTEST.cpp

--- INTEST.cpp
+++ INTEST.cpp
@@ -1,19 +1,23 @@
 //Dev Wadhwa
-#include <bits/stdc++.h>   //Used for Fast I/O
-using namespace std;       
-int main() {
+#include <bits/stdc++.h>   			//Used for Fast I/O
+using namespace std;      
+
+int main() 
+{
 	ios_base::sync_with_stdio(false);       //Used for Fast I/O
 	cin.tie(NULL);                          //Used for Fast I/O
-  int n, k;
-	cin >>n>>k;
+	int n, k;
+	cin>>n>>k;
 	int ans=0;
-	for (int i=0;i<n;i++) {
+	for (int i=0;i<n;i++) 
+	{
 		int t;
 		cin >> t;
-		if (t % k == 0) {
+		if (t % k == 0) 
+		{
 			ans++;
 		}		
 	}
-  cout << ans << "\n";		
-	return 0;
+  	cout<<ans<<"\n";                        //Better to use "\n" instead of endl for fast I/O		
+  	return 0;
 }
