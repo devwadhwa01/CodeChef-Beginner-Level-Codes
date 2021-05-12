b241ce8d072d8f8d745f378aab74a7e709e8df79
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-05-12 20:04:51 +0530

    Update FLOW002.cpp

--- FLOW002.cpp
+++ FLOW002.cpp
@@ -1,1 +1,19 @@
+//Dev Wadhwa
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    ios_base::sync_with_stdio(false);             //For Fast I/O
+    cin.tie(NULL);                                //For Fast I/O
+    int T;
+    cin>>T;
+    while(T!=0)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<a%b<<"\n";
+        T--;
+    }
+    return 0;
+}
