020ecd078bb915f62188b3c830551c3be664a790
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-05-12 19:22:48 +0530

    Update HS08TEST.cpp

--- HS08TEST.cpp
+++ HS08TEST.cpp
@@ -1,5 +1,5 @@
 //Dev Wadhwa
-#include <iomanip>
+#include <iomanip>					//To be able to use fixed and setprecision()
 #include <iostream>
 using namespace std;
 
@@ -11,14 +11,14 @@ int main()
 	if(amt%5==0 && amt<=(initial-0.5))
 	{
 	  final=initial-0.5-amt;
-	  cout<<fixed;
-	  cout<<setprecision(2);
-	  cout<<final;
+	  cout<<fixed;					//To round off to 2 decimal places
+	  cout<<setprecision(2);			//To round off to 2 decimal places
+	  cout<<final;					//To round off to 2 decimal places
 	}
 	else
 	{
-	  cout<<fixed;
-	  cout<<setprecision(2);
+	  cout<<fixed;					
+	  cout<<setprecision(2);			
 	  cout<<initial;
 	}
 	return 0;
