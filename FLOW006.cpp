35fcdf2ebaf26b899423df3c5fb853742fc3f6f1
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-05-12 20:00:38 +0530

    Update FLOW006.cpp

--- FLOW006.cpp
+++ FLOW006.cpp
@@ -1,1 +1,22 @@
+//Dev Wadhwa
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int T;
+    cin>>T;
+    while(T!=0)
+    {
+        int n,sum=0;
+        cin>>n;
+        for(int i=0;i<7;i++)
+        {
+            sum=sum+n%10;
+            n=n/10;
+        }
+        cout<<sum<<endl;
+        T--;
+    }
+    return 0;
+}
