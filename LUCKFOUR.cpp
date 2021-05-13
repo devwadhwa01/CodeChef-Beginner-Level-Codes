4fb5a5672444c64914eb134498726b3c2125b372
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-05-13 22:20:24 +0530

    Update LUCKFOUR.cpp

--- LUCKFOUR.cpp
+++ LUCKFOUR.cpp
@@ -1,1 +1,26 @@
+//Dev Wadhwa
+#include <iostream>
+using namespace std;
+#include<string.h>
+
+int main() {
+	int T;
+	cin>>T;
+	while(T!=0)
+	{
+	    char n[50];
+	    cin>>n;
+	    int c=0;
+	    for(int i=0;i<strlen(n);i++)
+	    {
+	        if(n[i]=='4')
+	        {
+	            c++;
+	        }
+	    }
+	    cout<<c<<"\n";
+	    T--;
+	}
+	return 0;
+}
 
