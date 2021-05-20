10c24586a6f1a7392f1a1b8759de42ada6d7615f
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-05-20 21:18:13 +0530

    Create FLOW008.cpp

---
+++ Coding Solutions/FLOW008.cpp
@@ -0,0 +1,19 @@
+//Dev Wadhwa
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T!=0)
+	{
+	    int n;
+	    cin>>n;
+	    if(n<10)
+	    cout<<"Thanks for helping Chef!\n";
+	    else
+	    cout<<"-1\n";
+	    T--;
+	}
+	return 0;
+}
